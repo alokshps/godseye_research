f739b89c61811a355118d76a76f400bd8a012c87
alokshps <alokshps@gmail.com>
2019-02-25 11:35:43 +0530

    email config remove

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -146,12 +146,12 @@ MEDIA_URL = '/media/'
 
 # Email Configration
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'godseye.research@gmail.com'
-EMAIL_HOST_PASSWORD = 'mahadev05021995'
-EMAIL_USE_TLS=True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'godseye.research@gmail.com'
+# EMAIL_HOST_PASSWORD = 'mahadev05021995'
+# EMAIL_USE_TLS=True
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
